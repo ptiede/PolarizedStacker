9123b52270b69a9879ae8e93557cee5de8893688
Paul Tiede <ptiede91@gmail.com>
2025-11-13 09:27:46 -0500

    Reduce the prior on the variability (prevent it from being too big)

--- stacker.jl
+++ stacker.jl
@@ -10,7 +10,7 @@ using TypedTables
 using VLBIImagePriors
 using LogDensityProblems
 
-const WT = Union{Truncated{EHTModelStacker.NormalFast{Float64}, Continuous, Float64, Float64, Float64}, DiagonalVonMises{Float64, Float64, Float64}}
+const WT = Union{Truncated{EHTModelStacker.NormalFast{Float64},Continuous,Float64,Float64,Float64},DiagonalVonMises{Float64,Float64,Float64}}
 
 struct SnapshotTarget{F,T}
     f::F
@@ -24,18 +24,18 @@ LogDensityProblems.dimension(d::SnapshotTarget) = d.ndim
 LogDensityProblems.capabilities(::Type{<:SnapshotTarget}) = LogDensityProblems.LogDensityOrder{0}()
 
 function make_marginal2(μ::T, σ::T, min, max, wrap)::WT where {T}
-    μT,σT,minT,maxT = promote(μ, σ, min, max)
+    μT, σT, minT, maxT = promote(μ, σ, min, max)
     !wrap && return truncated(EHTModelStacker.NormalFast(μT, σT), minT, maxT)::WT
     return DiagonalVonMises(μT, inv(σT^2))::WT
 end
 
-struct _SnapshotTransform{T, L}
+struct _SnapshotTransform{T,L}
     t::T
     l::L
 end
 
 function (st::_SnapshotTransform)(x)
-    any(x->(x<(0)||x>1), x) && return -1e300
+    any(x -> (x < (0) || x > 1), x) && return -1e300
     return st.l(HC.transform(st.t, x))
 end
 
@@ -44,14 +44,14 @@ end
 function EHTModelStacker.SnapshotWeights(θ, mins, maxs, wrapped, batchsize; scheduler=:serial)
     μ, σ = θ.μ, θ.σ
     dists = Vector{WT}(undef, length(μ))
-    dists .= make_marginal2.(μ,σ, mins, maxs, wrapped)
+    dists .= make_marginal2.(μ, σ, mins, maxs, wrapped)
     #dists = EHTModelStacker.NormalFast.(μ, σ)
     transition = EHTModelStacker.MyProduct(dists)#EHTModelStacker.MvNormalFast(μ, Σ.^2)
     prior = EHTModelStacker.MvUniform(mins, maxs)
     return SnapshotWeights(transition, prior, batchsize, scheduler)
 end
 
-struct BatchStackerLklhd{C, T, B}
+struct BatchStackerLklhd{C,T,B}
     chain::C
     min::T
     max::T
@@ -64,7 +64,7 @@ end
 
 
 function (l::BatchStackerLklhd)(θ)
-    ws = SnapshotWeights(θ, l.min, l.max, l.wrapped, l.batchsize; scheduler = l.scheduler)
+    ws = SnapshotWeights(θ, l.min, l.max, l.wrapped, l.batchsize; scheduler=l.scheduler)
     lapprox = lpdf(ws, l.chain)
     return lapprox
 end
@@ -88,11 +88,11 @@ end
 
 function _mkdf(echain, keys)
     df = DataFrame()
-    for (i,k) in enumerate(keys)
-        insertcols!(df, Symbol("mean_"*String(k))=>getindex.(echain.μ, i))
+    for (i, k) in enumerate(keys)
+        insertcols!(df, Symbol("mean_" * String(k)) => getindex.(echain.μ, i))
     end
-    for (i,k) in enumerate(keys)
-        insertcols!(df, Symbol("stdd_"*String(k))=>getindex.(echain.σ, i))
+    for (i, k) in enumerate(keys)
+        insertcols!(df, Symbol("stdd_" * String(k)) => getindex.(echain.σ, i))
     end
     df
 end
@@ -103,22 +103,22 @@ function readckpt(ckptfile)
 end
 
 function saveckpt(ckptfile, smplr, tv, logp, keys)
-    return save(ckptfile, Dict("smplr"=>smplr,
-                               "tv"=>tv,
-                               "logp"=>logp,
-                               "keys"=>keys))
+    return save(ckptfile, Dict("smplr" => smplr,
+        "tv" => tv,
+        "logp" => logp,
+        "keys" => keys))
 end
 
 
 function write_results(file, tv, logp, keys; stride=1)
     df = _mkdf(tv, keys)
     df.logp = logp
-    df[1:stride:end,:]|>CSV.write(file)
+    df[1:stride:end, :] |> CSV.write(file)
 end
 
 function extract_prior(df, names)
-    min  = Float64[]
-    max  = Float64[]
+    min = Float64[]
+    max = Float64[]
     wrap = Bool[]
     restrict = Bool[]
 
@@ -153,8 +153,8 @@ function read_prior_table(file)
     if !hasproperty(df, :restrict)
         df.restrict = fill("n", nrow(df))
     end
-    df.angular = map(x->ifelse(x=="n", false, true), df.angular)
-    df.restrict = map(x->ifelse(x=="n", false, true), df.restrict)
+    df.angular = map(x -> ifelse(x == "n", false, true), df.angular)
+    df.restrict = map(x -> ifelse(x == "n", false, true), df.restrict)
     df.name = Symbol.(df.name)
     return df
 end
